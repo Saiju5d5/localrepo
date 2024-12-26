1565a115cdb459ce9edf0d877a14810498a0d881
Saiju5d5 <22221a05d5@gmail.com>
2024-12-26 21:31:41 +0530

    add button

--- s.java
+++ s.java
@@ -3,6 +3,6 @@ class EX
     public static void main(String args[])
     {
         System.out.println("Hello world");
-        System.out.println("this is new feature");
+        System.out.println("this is new feature(button)");
     }
 }
